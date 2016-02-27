c18f5911678b24177c5fb59eede80ab0087bf0bb
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-02-27 16:53:30 -0600

    Shell of bugdemo test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'bugdemo' => 'static_pages#bugdemo'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
