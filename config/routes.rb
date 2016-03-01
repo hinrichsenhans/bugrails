1be471e89259facff022de826c2e1165ea02430c
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-02-29 22:11:48 -0600

    Layout tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'bugdemo' => 'static_pages#bugdemo'
 
   # The priority is based upon order of creation: first created -> highest priority.
