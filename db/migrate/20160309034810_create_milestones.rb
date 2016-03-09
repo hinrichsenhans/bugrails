286d6194db2ab33db6623eb28d54d8073a2a687b
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-03-09 13:21:39 -0600

    Components

--- db/migrate/20160309034810_create_milestones.rb
+++ db/migrate/20160309034810_create_milestones.rb
@@ -2,9 +2,9 @@ class CreateMilestones < ActiveRecord::Migration
   def change
     create_table :milestones do |t|
       t.string :name
-      add_foreign_key :products
-      
+
       t.timestamps null: false
     end
+
   end
 end
