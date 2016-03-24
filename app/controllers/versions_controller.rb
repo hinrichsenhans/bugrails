ec652d30f2b5fd4472f7a2e64301a38c785a7ae2
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-03-24 15:01:16 -0500

    Version implementation of Component editing

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,12 +2,60 @@ class VersionsController < ApplicationController
 
   before_action :set_product
 
+  def new
+    @version = Version.new(product: @product)
+  end
+
+  def create
+    @version = Version.new(allowed_params)
+    @version.product = @product
+    if @version.save
+      redirect_to action: 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @version = Version.find(params[:id])
+    @version.product = @product
+  end
+
+  def update
+    @version = Version.find(params[:id])
+    @version.product = @product
+    if @version.update(allowed_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @versions = @product.versions
+  end
+
+  def show
+    redirect_to action: 'index'
+  end
+
+  def destroy
+    @version = Version.find(params[:id])
+    @version.product = @product
+    if @version.destroy
+      redirect_to action: 'index'
+    else
+      redirect_to action: 'index'
+      #todo: more specific action later
+    end
   end
 
   private
     def set_product
       @product = Product.find(params[:product_id])
+    end
+
+    def allowed_params
+      params.require(:version).permit(:name, :product_id)
     end
 end
