c9fd8e401b53c056fd848c0e767514616855ddd4
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-03-24 23:23:40 -0500

    Added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, email: true
-  validates :password, length: { in: 8..30}
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :password, length: { in: 6..30}
 
 
   has_secure_password
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(:name => "Test Guy", 
+                     :email => "test@email.com", 
+                     :password => "bestpassword",
+                     :password_confirmation => "bestpassword")
+    @secondUser = User.new(:name => "Test Guy 2", 
+                     :email => "test2@email.com", 
+                     :password => "bestpassword",
+                     :password_confirmation => "bestpassword") 
+  end
+
+  test "user must have a name" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "user name must be unique" do
+    @secondUser.name = @user.name
+    assert @user.save
+    assert_not @secondUser.save
+    assert @user.destroy
+  end
+
+  test "user must have email" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "user email must be unique" do
+    @secondUser.email = @user.email
+
+    assert @user.save
+    assert_not @secondUser.save
+    assert @user.destroy
+
+  end
+
+  test "password must be 6 characters or longer" do
+    @user.password = "12345"
+    assert_not @user.valid?
+  end
+
+  test "password must be 20 characters or shorter" do
+    @user.password = "abcdefghijklmnopqrstu"
+    assert_not @user.valid?
+  end
+
 end
