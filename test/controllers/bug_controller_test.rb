89bc6cd755ce68caf8bb3881ed937a32f1011102
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-04-12 21:56:05 -0500

    Fix bug controller test problem

--- test/controllers/bug_controller_test.rb
+++ test/controllers/bug_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BugControllerTest < ActionController::TestCase
+class BugsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
