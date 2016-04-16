498d8531cdfc1bb6c989018ec5aff4232a1812c5
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-04-16 12:52:36 -0500

    Fix tests AGAIN

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # http://stackoverflow.com/questions/1574797/how-to-load-dbseed-data-into-test-database-automatically
-  puts "loading seeds"
   Rails.application.load_seed
-  puts Status.all.inspect
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
