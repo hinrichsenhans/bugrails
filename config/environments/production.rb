84250fca38c81a0580ce29fb4eaced1ea9178981
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2016-05-12 08:29:59 -0500

    Use sendgrid for activation email in production/heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
 
   host = 'agile-beach-24559.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
